6bd80611cfacaaf08a6fba2e98f66a68370a30be
Jerry <mikeynjerry@gmail.com>
2018-10-20 01:11:11 -0400

    Student Word Count

--- src/Student.js
+++ src/Student.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import ClusterWS from "clusterws-client-js";
 
 let api = "https://temp-vocacoord.herokuapp.com/api/";
 
 export class StudentScreen extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       classID: null
-    }
+    };
   }
 
   connectToClass() {
@@ -20,23 +19,19 @@ export class StudentScreen extends Component {
       this.socket = new ClusterWS({
         url: "wss://temp-vocacoord.herokuapp.com/"
       });
-      this.socket.on('connect', () => {
-        console.log('connected to the socket');
+      this.socket.on("connect", () => {
+        console.log("connected to the socket");
         this.channel = this.socket.subscribe(classID);
         const { navigate } = this.props.navigation;
         navigate("ClassScreen", { channel: this.channel });
-        //     this.channel.watch(msg => {
-        // console.log(`student heard this message: ${msg}`);
-        //   });
       });
-      this.socket.on('error', (err) => {
-        console.error('error: ', err);
+      this.socket.on("error", err => {
+        console.error("error: ", err);
       });
-      this.socket.on('disconnect', (code, reason) => {
+      this.socket.on("disconnect", (code, reason) => {
         console.log(`disconnected with code ${code} and reason ${reason}`);
         this.channel.unsubscribe();
       });
-
     }
   }
 
@@ -71,47 +66,41 @@ export class StudentScreen extends Component {
 export class ClassScreen extends React.Component {
   constructor(props) {
     super(props);
-    const { navigation } = props, channel = navigation.getParam('channel');
+    const { navigation } = props;
+    channel = navigation.getParam("channel");
     this.state = {
       words: []
-    }
-    channel.watch(msg => {		//Treats the end of the array as the most recent word said
-      console.log(`student heard this message: ${msg}`);
-      const words = this.state.words;
-      var index;
-      if ((index = words.indexOf(msg)) > -1) {
-        var temp = words[index]
-        for (var i = index; i < words.length - 1; i++)
-          words[i] = words[i + 1]
-
-        words[words.length - 1] = temp;
-      } else {
-        words.push(msg);
-      }
+    };
+    channel.watch(wordSaid => {
+      console.log(`student heard this message: ${wordSaid}`);
+      let words = [...this.state.words];
+      let word = words.find(w => w.word === wordSaid) || {
+        word: wordSaid,
+        count: 0
+      };
+      word.count += 1;
+      words = words.filter(w => w.word !== wordSaid);
+      words.unshift(word);
       this.setState({ words });
     });
 
-    const testing = ['test', 'test1', 'test2', 'test', 'test2', 'test3']
-    for (let word of testing) {
-      channel.publish(word)
-    }
+    const testing = ["test", "test1", "test2", "test", "test2", "test3"];
+    let idx = 0;
+    (function publish() {
+      channel.publish(testing[idx++]);
+      if (idx < testing.length) setTimeout(publish, 2000);
+    })();
   }
 
   render() {
     return (
       <View>
-        {
-          this.state.words.length > 0 &&
-          this.state.words.map((word, i) => {
-            return (
-              <Text>
-                {`${word}\n`}
-              </Text>
-            )
-          })
-        }
+        {this.state.words.length > 0 &&
+          this.state.words.map((w, i) => {
+            return <Text>{`${w.word} has been said ${w.count} times.\n`}</Text>;
+          })}
       </View>
-    )
+    );
   }
 }
 
