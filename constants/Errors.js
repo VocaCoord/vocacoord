7b8e7b820fc22a65063828f7ffebeb5a1d09b03f
Jerry <MikeynJerry@gmail.com>
2019-03-12 20:13:56 -0400

    Fetch Words From Firebase

---
+++ constants/Errors.js
@@ -0,0 +1,9 @@
+export const CODE_LENGTH = 'CODE_LENGTH';
+export const NO_CLASS = 'NO_CLASS';
+export const NO_WORDS = 'NO_WORDS';
+
+export const errors = {
+  CODE_LENGTH: 'Class codes must be 4 characters long.',
+  NO_CLASS: "That classroom doesn't exist.",
+  NO_WORDS: "Your teacher didn't add any words."
+};
--- screens/Connect/Connect.js
+++ screens/Connect/Connect.js
@@ -3,6 +3,13 @@ import { View, Text, ActivityIndicator } from 'react-native';
 import { Button, TextInput, HelperText } from 'react-native-paper';
 import { Divider } from 'react-native-elements';
 import ClusterWS from 'clusterws-client-js';
+import { classExists, getWords } from '../../firebase';
+import {
+  errors,
+  CODE_LENGTH,
+  NO_CLASS,
+  NO_WORDS
+} from '../../constants/Errors';
 
 let wentBack = false;
 export default class ConnectScreen extends Component {
@@ -11,7 +18,8 @@ export default class ConnectScreen extends Component {
     this.state = {
       classCode: '',
       isConnecting: false,
-      classCodeError: false
+      classCodeError: false,
+      errorCode: ''
     };
   }
 
@@ -24,88 +32,106 @@ export default class ConnectScreen extends Component {
     if (this.socket && this.socket.disconnect) this.socket.disconnect();
   }
 
-  isConnected = () => {
-    this.setState({ isConnecting: false });
-  };
+  isConnected = () => this.setState({ isConnecting: false });
 
   handleClassCodeChange = classCode =>
     this.setState({ classCode }, () => {
       const { classCodeError } = this.state;
-      if (classCodeError) this.validateClassCode();
+      if (classCodeError) this.validateCodeLength();
     });
 
-  connectToClass() {
-    if (!this.validateClassCode()) return;
-
+  validateClass = () => {
     const { classCode } = this.state;
 
-    this.setState({ isConnecting: true, classCodeError: false });
+    if (!this.validateCodeLength()) return;
+    this.setState({ isConnecting: true, classCodeError: false, errorCode: '' });
+    classExists(classCode).then(classroom => {
+      if (!classroom)
+        return this.setState({
+          isConnecting: false,
+          classCodeError: true,
+          errorCode: NO_CLASS
+        });
+      return getWords(classroom).then(words => {
+        if (words.length === 0)
+          return this.setState({
+            isConnecting: false,
+            classCodeError: true,
+            errorCode: NO_WORDS
+          });
+        return this.connectToClass(words);
+      });
+    });
+  };
 
+  connectToClass = words => {
+    const { classCode } = this.state;
     this.socket = new ClusterWS({
       url: 'wss://temp-vocacoord.herokuapp.com/'
     });
     this.socket.on('connect', () => {
-      this.channel = this.socket.subscribe(classCode.toUpperCase());
+      this.channel = this.socket.subscribe(classCode);
       const {
         navigation: { navigate }
       } = this.props;
-      setTimeout(() => {
-        if (wentBack) return;
-        navigate('ClassroomScreen', {
-          channel: this.channel,
-          callback: this.isConnected
-        });
-      }, 3000);
+      if (wentBack) return;
+      navigate('ClassroomScreen', {
+        channel: this.channel,
+        callback: this.isConnected,
+        words
+      });
     });
     this.socket.on('error', err => {
       console.error('error: ', err);
     });
     this.socket.on('disconnect', () => this.channel.unsubscribe());
-  }
+  };
 
-  validateClassCode() {
+  validateCodeLength = () => {
     const { classCode } = this.state;
+    let { errorCode } = this.state;
     let classCodeError = false;
-    if (!classCode || classCode.length !== 4) classCodeError = true;
-    this.setState({ classCodeError });
+    if (!classCode || classCode.length !== 4) {
+      classCodeError = true;
+      errorCode = CODE_LENGTH;
+    }
+    this.setState({ classCodeError, errorCode });
     return !classCodeError;
-  }
+  };
 
   render() {
     const { styles } = this.props;
-    const { isConnecting, classCode, classCodeError } = this.state;
+    const { isConnecting, classCode, classCodeError, errorCode } = this.state;
     return (
       <View style={styles.container}>
-        {isConnecting ? (
-          <View>
-            <Text>Hold on while we try to connect you to the classroom...</Text>
+        <View>
+          <TextInput
+            onBlur={this.validateCodeLength}
+            style={styles.textbox}
+            label="Class Code"
+            mode="outlined"
+            value={classCode}
+            onChangeText={this.handleClassCodeChange}
+            error={classCodeError}
+            autoCapitalize="characters"
+          />
+          <HelperText type="error" visible={classCodeError}>
+            {errors[errorCode]}
+          </HelperText>
+          <Divider style={styles.divider} />
+          {isConnecting ? (
             <ActivityIndicator size="large" color="#ffa500" />
-          </View>
-        ) : (
-          <View>
-            <TextInput
-              onBlur={this.validateClassCode}
-              style={styles.textbox}
-              label="Class Code"
-              mode="outlined"
-              value={classCode}
-              onChangeText={this.handleClassCodeChange}
-              error={classCodeError}
-            />
-            <HelperText type="error" visible={classCodeError}>
-              Classroom codes must be exactly 4 characters long.
-            </HelperText>
-            <Divider style={styles.divider} />
+          ) : (
             <Button
               color="#ffa500"
               style={styles.button}
               mode="contained"
-              onPress={() => this.connectToClass()}
+              onPress={() => this.validateClass()}
             >
               <Text style={styles.buttonText}>Connect</Text>
             </Button>
-          </View>
-        )}
+          )}
+        </View>
       </View>
     );
   }
