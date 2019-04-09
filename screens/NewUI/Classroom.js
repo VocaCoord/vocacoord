cdf460a769fb3e9a11b2059d64750ded7970c4d6
Jerry <mikeynjerry@gmail.com>
2019-04-09 16:12:07 -0400

    Fixed Collapsible Not Staying Open

--- screens/NewUI/Classroom.js
+++ screens/NewUI/Classroom.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -6,23 +6,24 @@ import {
   YellowBox,
   Image,
   ScrollView
-} from 'react-native';
-import { ListItem, Divider } from 'react-native-elements';
+} from "react-native";
+import { ListItem, Divider } from "react-native-elements";
 import {
   Collapse,
   CollapseHeader,
   CollapseBody
-} from 'accordion-collapse-react-native';
+} from "accordion-collapse-react-native";
 
-YellowBox.ignoreWarnings(['Setting']);
+YellowBox.ignoreWarnings(["Setting"]);
 
 export default class ClassScreen extends Component {
   constructor(props) {
     super(props);
     const { navigation } = props;
-    const channel = navigation.getParam('channel');
-    const words = navigation.getParam('words');
+    const channel = navigation.getParam("channel");
+    let words = navigation.getParam("words");
 
+    words = words.map(word => ({ ...word, collapsed: false }));
     channel.watch(wordSaid => {
       const { name } = wordSaid;
       const { words } = this.state;
@@ -38,71 +39,91 @@ export default class ClassScreen extends Component {
 
   componentWillMount() {
     const { navigation } = this.props;
-    navigation.addListener('didFocus', () => navigation.getParam('callback')());
+    navigation.addListener("didFocus", () => navigation.getParam("callback")());
   }
 
   componentWillUnmount() {
     const { navigation } = this.props;
-    navigation.getParam('channel').unsubscribe();
+    navigation.getParam("channel").unsubscribe();
   }
+
+  toggleCollapsed = name => isCollapsed => {
+    let { words } = this.state;
+    words = words.map(word => {
+      if (word.name === name) return { ...word, collapsed: isCollapsed };
+      return word;
+    });
+    return this.setState({ words });
+  };
+
   render() {
     const { words, currentWord } = this.state;
-    const wHeight = Dimensions.get('window').height;
+    const wHeight = Dimensions.get("window").height;
     const divHeight =
       wHeight / words.length / (words.length - 4 <= 0 ? 10 : words.length - 4);
     const { styles } = this.props;
     return (
       <ScrollView style={{ flexGrow: 1 }}>
-        {words.map(({ name, image, image: { url }, definition }, i) => (
-          <Collapse key={name}>
-            <CollapseHeader>
-              <ListItem
-                leftAvatar={{
-                  source: { uri: !image ? null : url }
-                }}
-                title={name}
-                titleStyle={{
-                  fontSize: 32,
-                  fontWeight: 'bold',
-                  color: currentWord.name === name ? 'red' : 'black'
-                }}
-              />
-              <Divider
-                style={{
-                  height: divHeight,
-                  backgroundColor: 'white'
-                }}
-              />
-            </CollapseHeader>
-            <CollapseBody>
-              <Divider style={{ backgroundColor: 'black', height: 2 }} />
-              <Divider
-                style={{
-                  height: 30,
-                  backgroundColor: 'white'
-                }}
-              />
+        {words.map(
+          ({ name, image: { url } = {}, definition, collapsed }, i) => {
+            return (
+              <Collapse
+                key={name}
+                isCollapsed={collapsed}
+                onToggle={this.toggleCollapsed(name)}
+              >
+                <CollapseHeader>
+                  <ListItem
+                    leftAvatar={{
+                      source: { uri: url ? url : null }
+                    }}
+                    title={name}
+                    titleStyle={{
+                      fontSize: 32,
+                      fontWeight: "bold",
+                      color: currentWord.name === name ? "red" : "black"
+                    }}
+                  />
+                  <Divider
+                    style={{
+                      height: divHeight,
+                      backgroundColor: "white"
+                    }}
+                  />
+                </CollapseHeader>
+                <CollapseBody>
+                  <Divider style={{ backgroundColor: "black", height: 2 }} />
+                  <Divider
+                    style={{
+                      height: 30,
+                      backgroundColor: "white"
+                    }}
+                  />
 
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Image
-                  source={{ uri: !image ? null : url }}
-                  style={{
-                    width: 200,
-                    height: 200
-                  }}
-                />
-                <Text style={styles.subStyle}>{definition}</Text>
-              </View>
-              <Divider
-                style={{
-                  height: 20,
-                  backgroundColor: 'white'
-                }}
-              />
-              <Divider style={{ backgroundColor: 'black', height: 2 }} />
-            </CollapseBody>
-          </Collapse>
-        ))}
+                  <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  >
+                    <Image
+                      source={{ uri: url ? url : null }}
+                      style={{
+                        width: 200,
+                        height: 200
+                      }}
+                    />
+                    <Text style={styles.subStyle}>{definition}</Text>
+                  </View>
+                  <Divider
+                    style={{
+                      height: 20,
+                      backgroundColor: "white"
+                    }}
+                  />
+                  <Divider style={{ backgroundColor: "black", height: 2 }} />
+                </CollapseBody>
+              </Collapse>
+            );
+          }
+        )}
       </ScrollView>
     );
   }
